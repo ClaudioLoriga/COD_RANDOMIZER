e0c1284f1c397457e17fd61ef49441854e3ec6de
Michele Cossu <m.cossu4@campus.unimib.it>
2022-06-18 00:27:51 +0200

    first commit

---
+++ app/src/main/java/com/example/cod_loadout_randomizer/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.cod_loadout_randomizer
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
